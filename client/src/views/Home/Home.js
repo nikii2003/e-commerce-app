d26394376179490cf079e6f2bd7990944c1a615c
Nikita kadiwike <ninavenikita492@gmail.com>
2023-10-23 16:58:48 +0530

    display product card with get request

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 function Home() {
+  const {id}=useParams();
+  const [product, setProduct] = useState([]);
+
+  const loadProduct = async () => {
+    const response = await axios.get("/products");
+    setProduct(response.data.data);
+  };
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1 className="text-center">Add Product</h1>
+
+      {product.map((product, index) => {
+        const { name, description, price, productImage, brand } = product;
+        return (
+          <div key={index}>
+            <div className="product-card-container">
+              <div>
+              <img src={productImage} className="product-image" />
+              </div>
+            
+            <div className="product-text">
+              <h1 className="product-name">{name}</h1>
+              <p className="product-description">{description}</p>
+              <p className="product-price">{price}</p>
+
+              <h3 className="product-brand">{brand}</h3>
+            </div>
+          </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
