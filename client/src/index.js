49c2cb51876e75eee533632a817ae97a6f784eee
Nikita kadiwike <ninavenikita492@gmail.com>
2023-10-23 14:59:10 +0530

    routing is added

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './views/Home/Home';
+import AddProduct from './views/AddProduct/AddProduct'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 
+const router=createBrowserRouter([{
+    path:'/',
+    element:<Home/>
+},
+{
+    path:'/addproduct',
+    element:<AddProduct/>
+}
+
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Home/>
+<RouterProvider router ={router}/>
 );
 
 
